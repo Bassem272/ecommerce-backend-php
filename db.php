86e485223babe8a8ff00ed5fbbd85c9b9e61ee2a
bassem <bassem.gehad95@yahoo.com>
2024-09-13 12:33:16 +0300

    First step!

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php 
+$hostname = "localhost";
+$username = "root";
+$userpassword = "newpassword";
+$dbname = "STUFFY";
+
+$conn = new mysqli($hostname, $username, $userpassword, $dbname);
+
+if( $conn-> connect_error){
+    die("connection failed". $conn->connect_error);
+}else{
+    echo "connection is successful";
+}
+;
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "here we go; Hello Bassem is here and just started ok lol Thanks and praise are to Allah the mighty who gave us his blessings. "
+
+?>
