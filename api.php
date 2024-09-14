9328dbdf62fcf6a8f0457bcad56dcf4c9ec49915
bassem <bassem.gehad95@yahoo.com>
2024-09-14 20:05:04 +0300

    adding api.php

---
+++ api.php
@@ -0,0 +1,20 @@
+<?php
+header("Content-Type: application/json");
+include "db.php";
+$sql  = "SELECT * FROM employees";
+
+$result = $conn -> query($sql);
+
+$employees = [];
+
+if ( $result->num_rows > 0){
+    while ($row = $result->fetch_assoc()){
+        $employees[] = $row;
+    }
+}
+
+ECHO json_encode(   $employees);  
+ECHO phpversion(); 
+
+?>
+<!-- 
